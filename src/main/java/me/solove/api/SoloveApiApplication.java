97421cad2be4e1fd2517c79427f3606f73f5b091
samabbott06 <samabbott06@gmail.com>
2025-06-18 02:55:59 -0700

    springboot/postgres framework

---
+++ src/main/java/me/solove/api/SoloveApiApplication.java
@@ -0,0 +1,15 @@
+package me.solove.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoloveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoloveApiApplication.class, args);
+	}
+
+}
+
+
